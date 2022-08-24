c6f54f35ad0323b7a32bb330a4d45ecc0b2c307e
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-08-24 17:35:26 +0430

    images and structure

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,5 +1,15 @@
-import "./styles.scss"
+import lilidooLogo from "../../../assets/images/header/lilydooo-logo-white.svg";
+import "./styles.scss";
 const HeaderComponent = () => {
-  return <header className="header">The headers</header>;
+  return (
+    <header className="header">
+      <div className="header__inner">
+        <div className="header__logo-container">
+          <img src={lilidooLogo} />
+        </div>
+        <div className="header__menu-container">Menu</div>
+      </div>
+    </header>
+  );
 };
 export default HeaderComponent;
