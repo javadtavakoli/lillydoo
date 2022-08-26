d6b5e4f55016e3cdf58a2ec95e44a0a1b70a6062
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-08-26 13:46:31 +0430

    added header menu

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,17 +1,41 @@
 import "./styles.scss";
+import shopIcon from "../../../assets/icons/basket.svg";
 const HeaderComponent = () => {
   return (
     <header className="header">
       <div className="header__inner">
         <div className="header__logo-container">
-          <a href="#" className="header__logo">
-            <img alt="Lillydoo Logo" src={"assets/images/header/lillydoo-logo-white.svg"} />
+          <a href="/" className="header__logo">
+            <img
+              alt="Lillydoo Logo"
+              src={"assets/images/header/lillydoo-logo-white.svg"}
+            />
           </a>
-          <a href="#" className="header__logo">
-            <img alt="Vivoy Logo" src={"assets/images/header/vivoy-logo-white.svg"} />
+          <a href="/" className="header__logo">
+            <img
+              alt="Vivoy Logo"
+              src={"assets/images/header/vivoy-logo-white.svg"}
+            />
           </a>
         </div>
-        <div className="header__menu-container">Menu</div>
+        <div className="header__menu-container">
+          <ul className="header__menu">
+            <li className="header__menu-item--drop-down">
+              <a href="/">DE</a>
+            </li>
+            <li>
+              <a href="/">Hilfe</a>
+            </li>
+            <li>
+              <a href="/">Login</a>
+            </li>
+            <li>
+              <a href="/">
+                <img alt="Shop Icon" src={shopIcon} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
