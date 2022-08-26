853776c8e6a132aa9560b2108e67fac7bf71d194
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-08-26 12:20:01 +0430

    added header logos

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -1,11 +1,15 @@
-import lilidooLogo from "../../../assets/images/header/lilydooo-logo-white.svg";
 import "./styles.scss";
 const HeaderComponent = () => {
   return (
     <header className="header">
       <div className="header__inner">
         <div className="header__logo-container">
-          <img src={lilidooLogo} />
+          <a href="#" className="header__logo">
+            <img alt="Lillydoo Logo" src={"assets/images/header/lillydoo-logo-white.svg"} />
+          </a>
+          <a href="#" className="header__logo">
+            <img alt="Vivoy Logo" src={"assets/images/header/vivoy-logo-white.svg"} />
+          </a>
         </div>
         <div className="header__menu-container">Menu</div>
       </div>
