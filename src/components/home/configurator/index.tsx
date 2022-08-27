42f063efe633cc844bddd8f34a124ef25aa28862
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-08-27 20:12:23 +0430

    added strong tag

--- src/components/home/configurator/index.tsx
+++ src/components/home/configurator/index.tsx
@@ -5,7 +5,11 @@ const ConfiguratorComponent = () => {
       <div className="configurator__title">4 Klicks zu Deinem Windel-Abo</div>
       <div className="configurator_inner">
         <div className="configurator__product-information">
-          <img src={"/assets/images/configurator/diapers-regular_size4.png"} alt="Dipers Regular Size 4" className="configurator__product-image"/>
+          <img
+            src={"/assets/images/configurator/diapers-regular_size4.png"}
+            alt="Dipers Regular Size 4"
+            className="configurator__product-image"
+          />
           <div className="configurator__product-description">
             Du willst mehr Auswahlmöglichkeiten?
           </div>
@@ -14,10 +18,18 @@ const ConfiguratorComponent = () => {
           </div>
           <div className="configurator__separator-line"></div>
           <ul className="configurator__product-features">
-            <li>6 Packungen, alle 4 Wochen</li>
-            <li>100% flexibel | jederzeit kündbar</li>
-            <li>Unsere hautfreundlichen Windeln</li>
-            <li>11,00 €/ Packung (0,38 €/Windel)</li>
+            <li>
+              6 Packungen, <strong>alle 4 Wochen</strong>
+            </li>
+            <li>
+              <strong>100% flexibel</strong>| jederzeit kündbar
+            </li>
+            <li>
+              Unsere <strong>hautfreundlichen</strong> Windeln
+            </li>
+            <li>
+              <strong>11,00 €</strong>/ Packung (0,38 €/Windel)
+            </li>
           </ul>
         </div>
       </div>
